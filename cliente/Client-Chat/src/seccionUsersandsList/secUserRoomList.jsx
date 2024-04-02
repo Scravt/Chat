f5d8e2bea5864e8d7ee40ec45b63e64c79d59db9
Gonzalo <34680110ax@gmail.com>
2024-04-02 13:59:03 -0300

    fix styles

--- cliente/Client-Chat/src/seccionUsersandsList/secUserRoomList.jsx
+++ cliente/Client-Chat/src/seccionUsersandsList/secUserRoomList.jsx
@@ -1,23 +1,26 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import styles from './SecUserRoomList.module.css'
 import { GoTriangleRight } from "react-icons/go";
 import { GoTriangleDown } from "react-icons/go";
 
-export const SecUserRoomList = ({data}) => {
-  const[showUsersList, setShowUsersList] = useState(false)
-  const {usersList} = data;  
-  //<GoTriangleRight />     <GoTriangleDown />
+export const SecUserRoomList = ({ data }) => {
+  const [showUsersList, setShowUsersList] = useState(false)
+  const { usersList } = data;
+  const CantidadUsuarios = usersList?.length;
   return (
     <div className={styles.ContainerUsersList}>
-       <div className={styles.title} onClick={()=>{setShowUsersList(showUsersList ?false:true)}} >{showUsersList ?<GoTriangleDown />:<GoTriangleRight />} {usersList?.length}  usuarios en linea  </div> 
-     {showUsersList &&
+      <div className={styles.title} onClick={() => { setShowUsersList(showUsersList ? false : true) }} >
+        {showUsersList ? <GoTriangleDown /> : <GoTriangleRight />}
+        {CantidadUsuarios}  {CantidadUsuarios<2?"usuario": "usuarios"}  en linea
+      </div>
+      {showUsersList &&
         <ul className={styles.UsersList}>
           {usersList?.map(user => {
             return <li key={user["id"]} className={styles.user} >{user["username"]}</li>
           })}
         </ul>
-     }
-      
+      }
+
     </div>
   )
 }
